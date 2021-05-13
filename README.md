4d41372449715fd1b7e661d81e3edeaa0789525a
theo923 <theotam912@gmail.com>
2021-05-14 07:50:33 +0800

    Updated for optimization && add transition when cursor touch button

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # My Personal Website
+
 	I am currently building my website.
 	I will try keeping things update every day!
 	and I will list the features that I have done 
-	and The Roadmap that any features that I want to achieve
+	and the Roadmap that any features that I want to achieve
 
 ## Features that working on
 
--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-let cursorTracker = document.querySelector("#cursor");
-let cursorTracker2 = document.querySelector("#cursor2");
-let cursorTracker3 = document.querySelector("#cursor3");
+let cursorTracker = Array.from(document.getElementsByClassName("cursor"));
 window.addEventListener('mousemove', cursor);
-
 function cursor(info){
-  cursorTracker.style.top = info.pageY + 'px';
-  cursorTracker.style.left = info.pageX + 'px';
-  cursorTracker2.style.top = info.pageY + 'px';
-  cursorTracker2.style.left = info.pageX + 'px';
-  cursorTracker3.style.top = info.pageY + 'px';
-  cursorTracker3.style.left = info.pageX + 'px';
+  cursorTracker.forEach(cursor => {
+    cursor.style.top = info.pageY + 'px';
+    cursor.style.left = info.pageX + 'px';
+  });
 }
 
-
-
-let barItem = document.getElementsByClassName('bar-item');
-Array.from(barItem).forEach((item) => {
+let barItem = Array.from(document.getElementsByClassName('bar-item'));
+barItem.forEach((item) => {
   let target = document.getElementById(item.id);
   target.addEventListener('mouseover', () =>{
-    cursorTracker.classList.add("link-grow");
+    cursorTracker.forEach(tracker => tracker.classList.add("link-grow"));
   });
   
   target.addEventListener('mouseleave', () =>{
-    cursorTracker.classList.remove("link-grow");
+    cursorTracker.forEach(tracker => tracker.classList.remove("link-grow"));
   });
 });
 
-let headingText = document.querySelector('.content-container .box1 .content-box .headingText p');
+
+
+let headingText = document.querySelector('#box1 .content-box .headingText');
 headingText.innerHTML = "Hello!";
-let mainText = document.querySelector('.content-container .box1 .content-box .mainText p');
+let mainText = document.querySelector('#box1 .content-box .mainText');
 mainText.innerHTML = "I am Theo Tam<br>Welcome to my page.<br>Let me introduce myself to you!";
+
+let headingText2 = document.querySelector('#box2 .content-box .headingText');
+headingText2.innerHTML = "He6456o!";
+let mainText2 = document.querySelector('#box2 .content-box .mainText');
+mainText2.innerHTML = "I am Theo Tam<br>Welcome to my page.<br>Let me introduce myself to you!";
