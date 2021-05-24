7a07093009f55546e38b0650dd986dd680b8de71
theo923 <theotam912@gmail.com>
2021-05-24 21:46:33 +0800

    Finished most of the features and Contents, and Bug fixes

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
 # My Personal Website
 
-	I am currently building my website.
-	I will try keeping things update every day!
-	and I will list the features that I have done 
-	and the Roadmap that any features that I want to achieve
+	I finished my website.
+	Starting from now, I will do the debug.
 	
 ## Content that working on
 
 | Content | Support |
 |---|:---:|
 | Homepage | ✔️ |
-| About Me | Drafted |
-| FAC | Drafted |
-| Trait | Drafted |
+| About Me | ✔️ |
+| FAC | ✔️ |
+| Trait | ✔️ |
 | Profile | ✔️ |
 
 ## Features that working on
@@ -20,10 +18,11 @@
 | Features | Support |
 |---|:---:|
 | Animated Background | ✔️ |
-| Special Cursor(dynamic color change) | ✔️ |
+| Special Cursor(with color change) | ✔️ |
 | Navigation Bar | ✔️ |
 | Animated Buttons | ✔️ |
 | Apply Color using Color Palette | ✔️ |
 | Dynamic Scroll Bar | ✔️ |
 | Rotation of Photo | ✔️ |
 | Image Carousel | ✔️ |
+| Responsive Design | ✔️ |
--- app.js
+++ app.js
@@ -100,7 +100,7 @@ barItem.forEach((item) => {                             // iterate every item
   target.addEventListener('click', () =>{          // trigger when the mouse leave from the button
     console.log(item.id);
     if(item.id === 'naviButton1'){ window.scrollTo(0,maxHeight);}                                         // scroll to the section
-    if(item.id === 'naviButton2'){ window.scrollTo(0,box3.offsetHeight + box4.offsetHeight);}             // scroll to the section
+    if(item.id === 'naviButton2'){ window.scrollTo(0,box2.offsetHeight + box3.offsetHeight + box4.offsetHeight);}             // scroll to the section
     if(item.id === 'naviButton3'){ window.scrollTo(0,box2.offsetHeight + box3.offsetHeight);}             // scroll to the section
     if(item.id === 'naviButton4'){ window.scrollTo(0,box2.offsetHeight);}                                 // scroll to the section
   });
@@ -214,14 +214,6 @@ icon.forEach((val, idx) => {
 let invisible = [];
 
 let backToTop = document.querySelector("#top");                                   // Top button
-backToTop.addEventListener("mouseover", () =>{                                    // trigger event if cursor move over the image
-  backToTop.src = "img/top2.png";                                                 // prompt user that the image can be clicked
-});
-
-backToTop.addEventListener("mouseleave", () =>{                                   // trigger event if cursor leave the image
-  backToTop.src = "img/top.jpg";                                                  // return to its original state
-});
-
 backToTop.addEventListener("click", () =>{                                        // trigger event if user clicked the image
   window.scrollTo(0,0);                                                           // scroll to the top of the page
 });
@@ -231,7 +223,7 @@ backToTop.addEventListener("click", () =>{                                      
 let img = Array.from(document.getElementsByClassName("image"));
 let imgDescription = document.querySelector("#imgDescription");
 let imageset = document.querySelector(".imageCarousel");
-let description = [''];
+let description = ['My Home Country', 'Famous Site in Hong Kong', 'The Unviersity I studied before', 'My Values', 'My Favourite City', 'My Dream Job', 'Way to achieve my dream', 'More detail 1', 'More detail 2', 'More detail 3'];
 let currentImageIndex = 0;
 imgDescription.innerHTML = description[currentImageIndex];
 
@@ -240,16 +232,21 @@ function imageCarousel () {
   if(currentImageIndex >= img.length - 1){ currentImageIndex = -1; }
   img[++currentImageIndex].classList.remove("hidden");
   imgDescription.innerHTML = description[currentImageIndex];
+  clearInterval(run);
+  run = setInterval( imageCarousel, 5000);
 }
 
 imageset.addEventListener("mouseover", addLinkGrow);
 imageset.addEventListener("mouseleave", removeLinkGrow);
 
+let run = setInterval( imageCarousel, 5000);
+
 img.forEach((val, idx) => {
   val.addEventListener("click", imageCarousel);
+
 })
 
-let run = setInterval( imageCarousel, 5000);
+
 
 /*==================== Text ====================*/
 
@@ -257,10 +254,25 @@ headingText1.innerHTML = "Hello!";
 mainText1.innerHTML = "My name is Theo Tam<br>Welcome to my page. <br>Let me introduce myself to you!";
 
 headingText2.innerHTML = "About Me";
+mainText2.innerHTML = "Hi! I am Theo. I am a HongKonger who is passionate on coding, have a good learning attitude, love exploring new things and accepting new challenges. <br><br>Last year, I was studying computer engineering in the Chinese University of Hong Kong. When I was living in the dorm, I have acquired communication skills during dorm activities. Besides, I embrace teamwork especially volunteering when people work together in helping people in need. " + 
+"<br><br>Although I cherish the learning experience in the university, I failed to proceed with my study due to some family issues. However, I don't want to give up on chasing my dream as a coder. " +
+"<br><br>Then, I got a chance for coming to UK with right to work. I thought it would be a great chance for me to step outside the comfort zone and have a greater stage in a fantastic city like London to realize my dream." +
+"<br><br>I have joined the community a few weeks ago, although it is just a short period of time, the experience is impressive and beneficial on my studying. " + 
+"As a member of the community, it would be a great pleasure if I could be involved in the coming cohort and I hope my abilities and passion would make contribution to the groupmates and achieve collective learning. ";
 
 headingText3.innerHTML = "Reasons for Applying next cohort";
-mainText3.innerHTML = "Here's are the 5 reasons that I would like to apply for the next cohort.";
+mainText3.innerHTML = "Here are the 5 reasons that I would like to apply for the next cohort.";
+valueText.innerHTML = "I really appreciate that Founders and Coders as a community that not just focus on coding, but also an organization with social responsibility (Tech for Better) and tend to promote social equality (attention on sexuality, people of color on tech industry). Therefore, I would like to become part of the project to pursue these values";
+communityText.innerHTML = "At first I found out that my code was untidy and clumsy when I was working on daily challenges, FAC mentors responded on my work. Instead of criticising or judging on that, they show appreciations on my work and encourage me to work better. I feel empowered and started to spend more time on improving my skills." + 
+"<br><br>Daily challenge is a good way for learning coding, everyone could achieve the same goal with different approachs. Everyday when I have finished my part, I would also spend more time to take a look on other's attempt as well. It is a valuable opportunity to learn from others on coding. ";
+attitudeText.innerHTML = "I am really glad that everybody shows their appreciation on each other’s work, it is encouraging and feeling not alone. " ;
+locationText.innerHTML = "London is an energetic and international city. It would be amazing if I could get a chance to work here. I believe it would be extremely helpful in my future career with more opportunities to get in touch with people in need." ;
+supportText.innerHTML = "Founders and Coders provides a great oppotunity for me to fulfill my dream for continuing my study and support me with employment, so that I could gain more confident in my future career path. Also, I can make some new friends from the community and integrate into the society.";
 
 headingText4.innerHTML = "Trait";
+invisible.push("My academic result was poor when I was studying in secondary school. When I discover my dream on computer, I taught myself using mind-map and find a way to proceed with my study.");
+invisible.push("I was a school football team member in secondary school. Through practices and competitions, I learnt about how amazing communication, cooperation and teamwork can be.");
+invisible.push("As I love different cultures and would like to explore more, I keep studying various languages, including English, Japanese, Mandarin and there would be more in the future.");
+invisible.push("I uphold fairness and peace. Therefore, I always act as a facilitator in a group as I am a good listener and willing to combine different ideas.");
 
 headingText5.innerHTML = "Profile";
